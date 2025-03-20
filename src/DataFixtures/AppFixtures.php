4da6f7fc93a9e20650a74fbb8f134528a5e1ef0c
Oamogetswe Mgidi <w696620@miway.co.za>
2025-03-20 13:58:16 +0200

    Created an app fixtures to load dummy data.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\VlogPost;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +10,25 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $vlogPost = new VlogPost();
+        $vlogPost->setTitle('A fourth vlog post');
+        $vlogPost->setPublished(new \DateTime('2025-03-20 13:51:00'));
+        $vlogPost->setContent('Its the fourth vlog post but with data fixtures this time.');
+        $vlogPost->setAuthor('Lentumetse Kutumela');
+        $vlogPost->setSlug('a-fourth-vlog-post');
 
+        $manager->persist($vlogPost);
+
+        $vlogPost = new VlogPost();
+        $vlogPost->setTitle('A fifth vlog post');
+        $vlogPost->setPublished(new \DateTime('2025-03-20 13:51:00'));
+        $vlogPost->setContent('Its the fifth vlog post but with data fixtures this time.');
+        $vlogPost->setAuthor('Stompie Kutumela');
+        $vlogPost->setSlug('a-fifth-vlog-post');
+
+        $manager->persist($vlogPost);
         $manager->flush();
+
+
     }
 }
